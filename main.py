b1b1e00b35ba282ff5496aaf1d2681a54438b7bd
Cristian Mihaiescu <cristian.mihaiescu@Cristians-MacBook-Pro.local>
2025-05-01 21:21:05 +0200

    I added the explanation and the script needed to replicate the dataset locally

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# This is a sample Python script.
-
-def hello_world():
-    return "Hello, World!"
-
-
-if __name__ == '__main__':
-    hello_world()
+# The script that is needed to get the data is:
+"""
+pip install xeno-canto
+xeno-canto -dl grp:"bats" q:"A"
+"""
+# grp: bats = stands for the group of animals we are filtering on
+# q: A = stands for the quality of the recording. The rank starts from A (clear and loud) to E (unclear)
+# Therefore, if we want also other qualities, we just select the same group but a different quality

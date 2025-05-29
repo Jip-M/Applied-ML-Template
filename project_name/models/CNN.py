Fixed a bug in the computation of intermediate accuracies. Made a change that allows us to work with multiple classes. Added a confusion matrix plot.

project_name/models/CNN.py
@@ -24,6 +24,7 @@ class AudioCNN(nn.Module):
 
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.Adam(self.parameters(), learning_rate)
+        self.num_classes = num_classes
         
 
     def forward(self, x: torch.tensor) -> torch.tensor:
@@ -84,17 +85,21 @@ class AudioCNN(nn.Module):
         Returns:
             dict: Contains both class indices and probabilities
         """
+        all_outputs = torch.empty(0, self.num_classes)
+        all_labels = torch.empty(0)
         with torch.no_grad():
             for inputs, labels in test_loader:
                 outputs = self(inputs)
+                all_outputs = torch.cat((all_outputs, outputs), dim=0)
+                all_labels = torch.cat((all_labels, labels), dim=0)
             
         # Get predicted class indices
-        _, predicted_classes = torch.max(outputs, dim=1)
+        _, predicted_classes = torch.max(all_outputs, dim=1)
         
         # Get probabilities (optional)
         # probabilities = torch.nn.functional.softmax(outputs, dim=1)
         
-        return predicted_classes, labels
+        return predicted_classes, all_labels
     
     def evaluate(self, predictions, labels):
         # predictions, labels = self.predict(test_loader)

project_name/models/pipeline.py
@@ -1,4 +1,3 @@
-import numpy as np
 from sklearn.model_selection import KFold
 from base_model import BaseModel
 import torch
@@ -12,18 +11,19 @@ import pandas as pd
 from metrics import MultiClassMetrics
 from CNN import AudioCNN
 from base_model import BaseModel
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
-
-def kfold_validation_CNN(X: np.ndarray, y: np.ndarray, k: int = 5) -> float:
-
+def kfold_validation_CNN(X: np.ndarray, y: np.ndarray, k: int = 5, num_classes: int = 2) -> (float, np.ndarray):
     kf = KFold(n_splits=k, shuffle=True, random_state=22)
     accuracies = []
+    cm = np.zeros((num_classes, num_classes))
 
     for train_index, test_index in kf.split(X):
         train_images, test_images = X[train_index], X[test_index]
         train_labels, test_labels = y[train_index], y[test_index]
 
-        num_epochs, model = initialize_CNN()
+        num_epochs, model = initialize_CNN(num_classes)
         train_data, train_labels_tensor = numpy_to_tensor(train_images, train_labels)
         test_data, test_labels_tensor = numpy_to_tensor(test_images, test_labels)
         batch_size = 32
@@ -37,18 +37,19 @@ def kfold_validation_CNN(X: np.ndarray, y: np.ndarray, k: int = 5) -> float:
         train_losses, train_accuracies = model.train_model(num_epochs, train_loader)
         predictions, labels = model.predict(test_loader)
         accuracy = model.evaluate(predictions, labels)
+        cm += confusion_matrix(labels, predictions, labels=np.arange(0, num_classes))
+        accuracies.append(accuracy)
         print(f"CNN Test Accuracy: {accuracy:.4f}")
         accuracies.append(accuracy)
         # model.plot_train_loss(num_epochs, train_losses, train_accuracies)
 
     average_accuracy = np.mean(accuracies)
     print(f"Average accuracy: {average_accuracy}")
-    return average_accuracy
+    return average_accuracy, cm
 
 
 def prepare_data():
-    lim = 220 # limit training data here
-
+    print("Preparing data")
     data_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data", "cleaned")
     label_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data", "labels.csv")
     label_array = pd.read_csv(label_file, header=None, names=["file_id", "species"], dtype={"str": "int"})["species"].to_numpy()
@@ -56,17 +57,13 @@ def prepare_data():
 
     images = []
     for i, file in enumerate(csv_files):
-        if i >= lim:
-            break
-
         img = pd.read_csv(file, header=None).values  # shape: (281, 1000)
         images.append(img)
 
-
     images = np.array(images)  # shape: (n_samples, 281, 1000)
     images = images[..., np.newaxis]  # Add channel dimension: (n_samples, 281, 1000, 1)
-    labels = label_array[:lim]
-    
+    labels = label_array
+    print(f"Loaded {len(csv_files)} sonograms")
     return images, labels # train_images, test_images, train_labels, test_labels (before)
 
 
@@ -76,11 +73,10 @@ def numpy_to_tensor(data, labels):
         return data_tensor, labels_tensor
     
 
-def initialize_CNN():
+def initialize_CNN(num_classes: int):
     torch.manual_seed(2222)
     np.random.seed(2222)
 
-    num_classes = 2 
     learning_rate = 0.001
     num_epochs = 10
 
@@ -106,8 +102,9 @@ def run_pipeline():
     #train_images, test_images, train_labels, test_labels = prepare_data()
     images, labels = prepare_data()
     #initialize_CNN()
-    kfold_validation_CNN(images, labels, k=5)
-
+    (acc, cm) = kfold_validation_CNN(images, labels, k=5, num_classes=4)
+    ConfusionMatrixDisplay(cm).plot()
+    plt.show()
     # run_CNN(train_images, test_images, train_labels, test_labels)
     # run_basemodel(train_images, test_images, train_labels, test_labels)
 
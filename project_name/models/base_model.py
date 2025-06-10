a4430dba6488b06daec739926c27738f2279e612
cristian.mihaiescu <cristimihaiescu@gmail.com>
2025-06-10 15:44:30 +0200

    Hypertunning and implementing the metrics, base model and made the code more modular.

--- project_name/models/base_model.py
+++ project_name/models/base_model.py
@@ -12,9 +12,9 @@ class BaseModel:
     def fit(self):
         self.model.fit(self.train_data[0], self.train_data[1])
 
-    def predict(self, X: np.ndarray):
-
-        return self.model.predict(X)
+    def predict(self):
+        probabilities = self.model.predict_proba(self.test_data[0])
+        return self.model.predict(self.test_data[0]), probabilities
     
     def prepare_data(self, train_images, train_labels, test_images, test_labels):
         # Flatten images for BaseModel: (N, 281, 1000, 1) -> (N, 281000)
